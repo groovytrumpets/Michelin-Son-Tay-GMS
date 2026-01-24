ae1fa9f19b207611ced075d1f65f1df663387c80
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-01-24 17:40:06 +0700

    initialize project

---
+++ src/main/java/com/g42/platform/gms/MstGmsApplication.java
@@ -0,0 +1,13 @@
+package com.g42.platform.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstGmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstGmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g42/platform/gms/MstGmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g42.platform.gms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstGmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
