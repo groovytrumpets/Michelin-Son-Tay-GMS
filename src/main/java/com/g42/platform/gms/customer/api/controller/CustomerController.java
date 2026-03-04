94c12cc6ffb03bb76e9b69ad124d4805aa9de574
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-03-04 13:59:08 +0700

    refactor: create API

--- src/main/java/com/g42/platform/gms/customer/api/controller/CustomerController.java
+++ src/main/java/com/g42/platform/gms/customer/api/controller/CustomerController.java
@@ -1,6 +1,11 @@
 package com.g42.platform.gms.customer.api.controller;
 
+import com.g42.platform.gms.common.dto.ApiResponse;
+import com.g42.platform.gms.common.dto.ApiResponses;
+import com.g42.platform.gms.customer.api.dto.CustomerCreateDto;
+import com.g42.platform.gms.customer.application.service.CustomerService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @RequestMapping("/api/customer/")
 public class CustomerController {
-//    @PostMapping("create")
-//    public ResponseEntity<ApiResponse<CustomerCreateDto>> createCustomer(@RequestBody CustomerCreateDto customerDto) {
-//
-//        return ResponseEntity.ok(ApiResponses.success(bookingService.confirmBookingRequest(requestId)));
-//    }
+    @Autowired
+    CustomerService customerService;
+    @PostMapping("create")
+    public ResponseEntity<ApiResponse<CustomerCreateDto>> createCustomer(@RequestBody CustomerCreateDto customerDto) {
+
+        return ResponseEntity.ok(ApiResponses.success(customerService.createNewCustomer(customerDto)));
+    }
 }
