7771d23ad17d0b1c4897f88d70fecec5a499532f
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-09 13:59:49 +0700

    Feature/advisor estimation (#66)
    * feat: initialize advisor-estimate
    
    * refactor: mapping naming wrong
    
    * update: estimate enums
    
    * update: Zalo Login connected, vps test required

--- src/main/java/com/g42/platform/gms/notification/api/controller/NotificationTestController.java
+++ src/main/java/com/g42/platform/gms/notification/api/controller/NotificationTestController.java
@@ -1,18 +1,25 @@
 package com.g42.platform.gms.notification.api.controller;
 
+import com.g42.platform.gms.notification.api.dto.TokenRes;
 import com.g42.platform.gms.notification.application.service.NotificationService;
 import com.g42.platform.gms.notification.domain.NotificationSender;
 import com.g42.platform.gms.notification.infrastructure.PKCE;
 import com.g42.platform.gms.notification.infrastructure.ZaloOAuthService;
+import com.g42.platform.gms.notification.infrastructure.entity.ZaloToken;
+import com.g42.platform.gms.notification.infrastructure.repository.ZaloTokenRepo;
+import com.nimbusds.oauth2.sdk.TokenResponse;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/test/notify/")
+@RequestMapping("/zalo")
 public class NotificationTestController {
     private final NotificationService notificationSender;
 
@@ -26,7 +33,7 @@ public class NotificationTestController {
         return "OK";
     }
     private ZaloOAuthService zaloOAuthService;
-    @GetMapping("/zalo/connect")
+    @GetMapping("/login")
     public void connectZalo(HttpServletResponse response) throws IOException {
 
         String verifier = PKCE.generateVerifier();
@@ -34,5 +41,23 @@ public class NotificationTestController {
         String url = zaloOAuthService.buildAuthorizeUrl();
 
         response.sendRedirect(url);
+    }
+    private ZaloTokenRepo zaloTokenRepo;
+    @GetMapping("/callback")
+    public ResponseEntity<String> callback(@RequestParam String code,
+                                           @RequestParam String state){
+        //todo: find record
+        ZaloToken zaloToken = zaloTokenRepo.findByState(state);
+        if (zaloToken == null) {
+            return ResponseEntity.badRequest().body("Invalid state");
+        }
+        String codeVerifier = zaloToken.getCodeVerifier();
+        //todo: call zalo api to save token
+        TokenRes token = zaloOAuthService.getAccessToken(code, codeVerifier);
+        //todo: save token to db
+        zaloToken.setAccessToken(token.getAccess_token());
+        zaloToken.setRefreshToken(token.getRefresh_token());
+        zaloTokenRepo.save(zaloToken);
+        return ResponseEntity.ok("Zalo connected successfully");
     }
 }
