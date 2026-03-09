d72a4731458b9cbb58c4b8802d8e47ab86f7ebec
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-09 12:50:31 +0700

    Feature/zalo notification (#62)
    * feat: initialize Zalo connect
    
    * update: Zalo connect

--- src/main/java/com/g42/platform/gms/notification/api/controller/NotificationTestController.java
+++ src/main/java/com/g42/platform/gms/notification/api/controller/NotificationTestController.java
@@ -2,11 +2,13 @@ package com.g42.platform.gms.notification.api.controller;
 
 import com.g42.platform.gms.notification.application.service.NotificationService;
 import com.g42.platform.gms.notification.domain.NotificationSender;
+import com.g42.platform.gms.notification.infrastructure.PKCE;
+import com.g42.platform.gms.notification.infrastructure.ZaloOAuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @AllArgsConstructor
@@ -22,5 +24,15 @@ public class NotificationTestController {
                 "10h"
         );
         return "OK";
+    }
+    private ZaloOAuthService zaloOAuthService;
+    @GetMapping("/zalo/connect")
+    public void connectZalo(HttpServletResponse response) throws IOException {
+
+        String verifier = PKCE.generateVerifier();
+        String challenge = PKCE.generateChallenge(verifier);
+        String url = zaloOAuthService.buildAuthorizeUrl();
+
+        response.sendRedirect(url);
     }
 }
--- src/main/java/com/g42/platform/gms/notification/infrastructure/ZaloOAuthService.java
+++ src/main/java/com/g42/platform/gms/notification/infrastructure/ZaloOAuthService.java
@@ -1,63 +1,20 @@
 package com.g42.platform.gms.notification.infrastructure;
 
-import com.g42.platform.gms.notification.api.dto.TokenRes;
-import com.g42.platform.gms.notification.infrastructure.entity.ZaloToken;
-import com.g42.platform.gms.notification.infrastructure.repository.ZaloTokenRepo;
-import com.nimbusds.oauth2.sdk.TokenResponse;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-@AllArgsConstructor
+
 @Service
 public class ZaloOAuthService {
-    private ZaloTokenRepo zaloTokenRepo;
     public String buildAuthorizeUrl() {
 
         String verifier = PKCE.generateVerifier();
         String challenge = PKCE.generateChallenge(verifier);
-        System.out.println("Verifier: "+verifier);
-        System.out.println("Challenge: "+challenge);
 
         // TODO: lưu verifier (redis/db)
-        ZaloToken zaloToken = new ZaloToken();
-        zaloToken.setAccessToken(verifier);
-        zaloToken.setState("verified");
-        zaloTokenRepo.save(zaloToken);
+
         return "https://oauth.zaloapp.com/v4/oa/permission"
-                + "?app_id=1162973287649757116"
-                + "&redirect_uri=https://michelinsontay.vn/zalo/callback"
+                + "?app_id=YOUR_APP_ID"
+                + "&redirect_uri=YOUR_CALLBACK_URL"
                 + "&code_challenge=" + challenge
                 + "&code_challenge_method=S256";
-    }
-
-    public TokenRes getAccessToken(String code, String codeVerifier) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("app_id", "1162973287649757116");
-        body.add("grant_type", "authorization_code");
-        body.add("code", code);
-        body.add("code_verifier", codeVerifier);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity<?> request = new HttpEntity<>(body, headers);
-
-        ResponseEntity<TokenRes> response =
-                restTemplate.postForEntity(
-                        "https://oauth.zaloapp.com/v4/access_token",
-                        request,
-                        TokenRes.class
-                );
-
-        return response.getBody();
     }
 }
