3222789bea406007c63caff64ac2e0b3bc1bcc51
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-17 20:57:39 +0700

    Feature/admin staff management (#92)
    * feat: staff view all
    
    * update: staff view all and search, paging, filter by date, is active, roleId
    
    * fix: admin customer search and filter
    
    * fix: admin customer search and filter
    
    * feat: get Staff Profile by Id
    
    * feat: get Customer Profile by Id
    
    * update: status for customer and staff filter
    
    * feat: create staff profile and staff auth
    
    * feat: update staff profile

--- src/main/java/com/g42/platform/gms/staff/profile/api/controller/StaffController.java
+++ src/main/java/com/g42/platform/gms/staff/profile/api/controller/StaffController.java
@@ -4,7 +4,9 @@ import com.g42.platform.gms.auth.entity.StaffProfile;
 import com.g42.platform.gms.common.dto.ApiResponse;
 import com.g42.platform.gms.common.dto.ApiResponses;
 import com.g42.platform.gms.staff.profile.api.dto.RoleDto;
+import com.g42.platform.gms.staff.profile.api.dto.StaffCreateDto;
 import com.g42.platform.gms.staff.profile.api.dto.StaffProfileDto;
+import com.g42.platform.gms.staff.profile.api.dto.StaffUpdateDto;
 import com.g42.platform.gms.staff.profile.app.service.StaffService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,5 +39,14 @@ public class StaffController {
     @GetMapping("all-roles")
     public ResponseEntity<ApiResponse<List<RoleDto>>> getAllRoles() {
         return ResponseEntity.ok(ApiResponses.success(staffService.getListOfRoles()));
+    }
+    @PostMapping("create")
+    public ResponseEntity<ApiResponse<StaffProfileDto>> createStaffProfile(@RequestBody StaffCreateDto staffProfileDto) {
+        return ResponseEntity.ok(ApiResponses.success(staffService.createStaff(staffProfileDto)));
+    }
+    @PutMapping("{staffId}/update")
+    public ResponseEntity<ApiResponse<StaffProfileDto>> updateStaffProfile(@PathVariable Integer staffId,
+                                                                           @RequestBody StaffUpdateDto staffProfileDto) {
+        return ResponseEntity.ok(ApiResponses.success(staffService.updateStaff(staffId, staffProfileDto)));
     }
 }
