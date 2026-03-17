1aaa034fa484c96804465863fa85977f634832b4
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-17 22:12:43 +0700

    Customer (#93)
    * initialize: Staff create customer profile
    
    * fix: staff have multiple roles
    
    * fix: vps deploy bugs
    
    * refactor: create API
    
    * feat: create customerAccount
    
    * feat: list of customer
    
    * fix: service id convert to catalog id
    
    * fix: catalog id added in service respond
    
    * feat: get Customer Profile by Id
    
    * fix: customer status return added
    
    * fix: customer status return added

--- src/main/java/com/g42/platform/gms/customer/api/dto/CustomerProfileDto.java
+++ src/main/java/com/g42/platform/gms/customer/api/dto/CustomerProfileDto.java
@@ -1,8 +1,7 @@
-package com.g42.platform.gms.customer.domain.entity;
+package com.g42.platform.gms.customer.api.dto;
 
 import com.g42.platform.gms.auth.entity.CustomerStatus;
 import com.g42.platform.gms.auth.entity.Gender;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,12 +9,11 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CustomerProfile {
+public class CustomerProfileDto {
     private Integer customerId;
     private String fullName;
     private String phone;
@@ -23,7 +21,7 @@ public class CustomerProfile {
     private LocalDate dob;
     private Gender gender;
     private String avatar;
-    private CustomerStatus status;
     private LocalDateTime firstBookingAt;
     private LocalDateTime createdAt;
+    private CustomerStatus status;
 }
