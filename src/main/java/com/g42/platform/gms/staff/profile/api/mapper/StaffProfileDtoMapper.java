da84d39acf1dea844d2edddc42cc1d972b15b519
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-17 10:21:43 +0700

    Feature/admin staff management (#86)
    * feat: staff view all
    
    * update: staff view all and search, paging, filter by date, is active, roleId
    
    * fix: admin customer search and filter
    
    * fix: admin customer search and filter

--- src/main/java/com/g42/platform/gms/staff/profile/api/mapper/StaffProfileDtoMapper.java
+++ src/main/java/com/g42/platform/gms/staff/profile/api/mapper/StaffProfileDtoMapper.java
@@ -5,10 +5,12 @@ import com.g42.platform.gms.staff.profile.api.dto.StaffProfileDto;
 import com.g42.platform.gms.staff.profile.domain.entity.Role;
 import com.g42.platform.gms.staff.profile.domain.entity.StaffProfile;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface StaffProfileDtoMapper {
     StaffProfileDto toStaffProfileDto(StaffProfile staffProfile);
     StaffProfile toStaffProfile(StaffProfileDto staffProfileDto);
+    @Mapping(source = "id", target = "roleId")
     RoleDto toDto(Role role);
 }
