02f91345f6868c0e7cd130c08bccb927857d9db0
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-17 10:54:31 +0700

    Feature/admin staff management (#90)
    * feat: staff view all
    
    * update: staff view all and search, paging, filter by date, is active, roleId
    
    * fix: admin customer search and filter
    
    * fix: admin customer search and filter
    
    * feat: get Staff Profile by Id
    
    * feat: get Customer Profile by Id

--- src/main/java/com/g42/platform/gms/staff/profile/infrastructure/mapper/StaffProfileJpaMapper.java
+++ src/main/java/com/g42/platform/gms/staff/profile/infrastructure/mapper/StaffProfileJpaMapper.java
@@ -1,6 +1,8 @@
 package com.g42.platform.gms.staff.profile.infrastructure.mapper;
 
+import com.g42.platform.gms.staff.profile.domain.entity.Role;
 import com.g42.platform.gms.staff.profile.domain.entity.StaffProfile;
+import com.g42.platform.gms.staff.profile.infrastructure.entity.RoleJpa;
 import com.g42.platform.gms.staff.profile.infrastructure.entity.StaffProfileJpa;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,4 +12,5 @@ public interface StaffProfileJpaMapper {
     @Mapping(source = "staffAuth.email", target = "email")
     @Mapping(source = "staffAuth.status", target = "status")
     StaffProfile toDomain(StaffProfileJpa domain);
+    Role toRoleDomain(RoleJpa domain);
 }
