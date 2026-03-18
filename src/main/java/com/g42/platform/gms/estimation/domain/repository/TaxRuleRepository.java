91a5e1b2e478c12234060bef404ed327d000b177
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-03-18 16:42:04 +0700

    feat: view estimateItem include tax rule

--- src/main/java/com/g42/platform/gms/estimation/domain/repository/TaxRuleRepository.java
+++ src/main/java/com/g42/platform/gms/estimation/domain/repository/TaxRuleRepository.java
@@ -4,9 +4,12 @@ import com.g42.platform.gms.estimation.domain.entity.TaxRule;
 import com.g42.platform.gms.estimation.domain.entity.WorkCategory;
 
 import java.util.List;
+import java.util.Map;
 
 public interface TaxRuleRepository {
     TaxRule findById(Integer taxRuleId);
 
     List<TaxRule> getAllActiveTax();
+
+    List<TaxRule> findAllByIds(List<Integer> taxRuleIds);
 }
--- src/main/java/com/g42/platform/gms/estimation/infrastructure/TaxRuleRepositoryImpl.java
+++ src/main/java/com/g42/platform/gms/estimation/infrastructure/TaxRuleRepositoryImpl.java
@@ -8,7 +8,9 @@ import com.g42.platform.gms.estimation.infrastructure.repository.TaxRuleReposito
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @AllArgsConstructor
@@ -26,6 +28,14 @@ public class TaxRuleRepositoryImpl implements TaxRuleRepository {
     public List<TaxRule> getAllActiveTax() {
         List<TaxRuleJpa> taxRuleJpas = taxRepositoryJpa.findAllByIsActive((byte)1);
         return taxRuleJpas.stream().map(taxJpaMapper::toDomain).toList();
+    }
+
+    @Override
+    public List<TaxRule> findAllByIds(List<Integer> taxRuleIds) {
+        return taxRepositoryJpa.findAllById(taxRuleIds)
+                .stream()
+                .map(taxJpaMapper::toDomain)
+                .toList();
     }
 }
 
