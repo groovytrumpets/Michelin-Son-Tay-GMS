74d2de1d0aaf9b9e0ec0e3b15ae0ee2a9b647271
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-18 10:45:36 +0700

    feat: view all available staff and assign staff (#97)

---
+++ src/main/java/com/g42/platform/gms/service_ticket_management/api/dto/assign/AvailableStaffDto.java
@@ -0,0 +1,20 @@
+package com.g42.platform.gms.service_ticket_management.api.dto.assign;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvailableStaffDto {
+    private Integer staffId;
+    private String fullName;
+    private String phone;
+    private String avatar;
+    private List<RoleDto> roles;
+}
--- src/main/java/com/g42/platform/gms/service_ticket_management/application/service/TicketAssignmentService.java
+++ src/main/java/com/g42/platform/gms/service_ticket_management/application/service/TicketAssignmentService.java
@@ -1,9 +1,24 @@
 package com.g42.platform.gms.service_ticket_management.application.service;
 
+import com.g42.platform.gms.service_ticket_management.api.dto.assign.AssignStaffDto;
+import com.g42.platform.gms.service_ticket_management.api.dto.assign.AvailableStaffDto;
+import com.g42.platform.gms.service_ticket_management.api.dto.assign.RoleDto;
+import com.g42.platform.gms.service_ticket_management.domain.repository.TicketAssignmentRepo;
+import com.g42.platform.gms.staff.profile.infrastructure.entity.StaffProfileJpa;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TicketAssignmentService {
+    private final TicketAssignmentRepo ticketAssignmentRepo;
+    public List<AvailableStaffDto> getAvailableStaff(Integer ticketId, String role) {
+        return ticketAssignmentRepo.getAvailableStaff(ticketId, role);
+    }
+
+    public AssignStaffDto assignStaff(Integer ticketId, AssignStaffDto dto) {
+        return ticketAssignmentRepo.assignStaff(ticketId, dto);
+    }
 }
