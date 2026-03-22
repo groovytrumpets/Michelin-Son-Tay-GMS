ebbb992d6d96193311bd42f7ccf81b75ad0307f1
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-03-22 18:28:16 +0700

    update: validate create promotion

--- src/main/java/com/g42/platform/gms/promotion/domain/exception/PromotionErrorCode.java
+++ src/main/java/com/g42/platform/gms/promotion/domain/exception/PromotionErrorCode.java
@@ -1,7 +1,9 @@
 package com.g42.platform.gms.promotion.domain.exception;
 
 public enum PromotionErrorCode {
-    INVALID_ID,
+    INVALID_ENUM_APPLY_TO,
+    INVALID_ENUM_TYPE,
+    INVALID_ENUM_TARGET_TYPE,
     INVALID_CONTACT_INFO,
     INVALID_PHONE,
     INVALID_DOB,
