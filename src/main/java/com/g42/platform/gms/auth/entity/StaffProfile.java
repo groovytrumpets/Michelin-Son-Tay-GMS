ff8b7da5233bc3be4c4fedbe646b9cd16fc49ade
tanle04 <tan07112004@gmail.com>
2026-03-23 23:02:00 +0700

    fix: update JPA StaffProfile

--- src/main/java/com/g42/platform/gms/auth/entity/StaffProfile.java
+++ src/main/java/com/g42/platform/gms/auth/entity/StaffProfile.java
@@ -19,9 +19,6 @@ public class StaffProfile {
     private String gender;
     private java.sql.Date dob;
     private String avatar;
-    private String email;
-    private String employmentStatus;
-    private java.sql.Date hireDate;
     private java.sql.Timestamp createdAt;
 
     @OneToOne(mappedBy = "staffProfile")
