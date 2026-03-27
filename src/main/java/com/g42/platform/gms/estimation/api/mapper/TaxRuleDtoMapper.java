fcd350ee2dc97ebed26c5bbc96842112a2e02b69
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-27 17:04:43 +0700

    Update/estimate (#132)
    * fix: update estimate item, update small item
    
    * fix: update estimate item, update small item
    
    * fix: update estimate item, update small item
    
    * update: estimateTax by Workcategory
    
    * feat: create Taxule

--- src/main/java/com/g42/platform/gms/estimation/api/mapper/TaxRuleDtoMapper.java
+++ src/main/java/com/g42/platform/gms/estimation/api/mapper/TaxRuleDtoMapper.java
@@ -3,6 +3,7 @@ package com.g42.platform.gms.estimation.api.mapper;
 import com.g42.platform.gms.estimation.api.dto.EstimateItemDto;
 import com.g42.platform.gms.estimation.api.dto.EstimateRespondDto;
 import com.g42.platform.gms.estimation.api.dto.WorkCataDto;
+import com.g42.platform.gms.estimation.api.dto.taxRule.TaxCreateDto;
 import com.g42.platform.gms.estimation.api.dto.taxRule.TaxRuleDto;
 import com.g42.platform.gms.estimation.domain.entity.Estimate;
 import com.g42.platform.gms.estimation.domain.entity.EstimateItem;
@@ -16,4 +17,5 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TaxRuleDtoMapper {
     TaxRuleDto toTaxRuleDto(TaxRule taxRule);
+    TaxRule toTaxRule(TaxCreateDto taxCreateDto);
 }
