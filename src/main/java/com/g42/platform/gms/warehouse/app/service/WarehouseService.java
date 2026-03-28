3b2744ff47ca2b1f4b61f6075fcc194ab9cd012d
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-03-28 17:52:13 +0700

    Feature/warehouse (#137)
    * feat: initialize warehouse
    
    * feat: initialize warehouse
    
    * feat: initialize warehouse
    
    * feat: get hint catalogItem while create
    
    * fix: wrong enum
    
    * fix: catalog view home
    
    * fix: catalog view home
    
    * feat: catalog create new
    
    * feat: service create new
    
    * feat: create Item Category and update catalogCreate
    
    * update: fixbugs
    
    * update: item category getall
    
    * update: update catalog_name with list of spec by catalogId
    
    * feat: fillter and search catalogItem

--- src/main/java/com/g42/platform/gms/warehouse/app/service/WarehouseService.java
+++ src/main/java/com/g42/platform/gms/warehouse/app/service/WarehouseService.java
@@ -1,15 +1,28 @@
 package com.g42.platform.gms.warehouse.app.service;
 
+import com.g42.platform.gms.warehouse.api.dto.CatalogSummaryDto;
+import com.g42.platform.gms.warehouse.api.mapper.CatalogDtoMapper;
+import com.g42.platform.gms.warehouse.domain.entity.CatalogItem;
+import com.g42.platform.gms.warehouse.domain.enums.CatalogItemType;
 import com.g42.platform.gms.warehouse.domain.repository.CatalogItemRepo;
 import com.g42.platform.gms.warehouse.domain.repository.WarehouseRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class WarehouseService {
     @Autowired
     private WarehouseRepo warehouseRepo;
+    @Autowired
+    private CatalogDtoMapper catalogDtoMapper;
 
 
+    public Page<CatalogSummaryDto> getListItems(int page, int size, CatalogItemType itemType, Boolean isActive, String search, Integer brandId, Integer productLineId, Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String sortBy) {
+        Page<CatalogItem> catalogItems = warehouseRepo.getListOfCatalogItems(page,size,itemType,isActive,search,brandId,productLineId,categoryId,minPrice,maxPrice,sortBy);
+        return catalogItems.map(catalogDtoMapper::toSumaryDto);
+    }
 }
