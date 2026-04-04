f01d88f4600dadc45af05a8ac99280a9a352d9d8
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-04-04 19:08:36 +0700

    Feature/zalo intergarion (#181)
    * feat: integration to Zalo and send notification
    
    * update: otp and feedback sender
    
    * update: Zalo webhook receive feedback

--- src/main/java/com/g42/platform/gms/notification/application/service/NotificationService.java
+++ src/main/java/com/g42/platform/gms/notification/application/service/NotificationService.java
@@ -3,6 +3,9 @@ package com.g42.platform.gms.notification.application.service;
 import com.g42.platform.gms.notification.domain.NotificationSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 public class NotificationService {
     private final NotificationSender notificationSender;
@@ -11,7 +14,15 @@ public class NotificationService {
         this.notificationSender = notificationSender;
     }
 
-    public void sendBookingCf(String s, String nguyenVanA, String s1) {
-        notificationSender.sendBookingCf(s, nguyenVanA, s1);
+    public void sendBookingCf(String phone, String name, List<String> productName, String orderCode, LocalDateTime bookingTime, String garageLocation) {
+        notificationSender.sendBookingConfirm(phone, name, productName,orderCode,bookingTime,garageLocation);
+    }
+
+    public void sendOtpViaZalo(String number,String otp) {
+        notificationSender.sendOtpVerify(number,otp);
+    }
+
+    public void sendFeedbackViaZalo(String number, String name, String code) {
+        notificationSender.sendFeedback(number,name,code);
     }
 }
