801ea51940ec07981b0f88f53bd4ab781202393d
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-04-05 00:19:55 +0700

    Feature/zalo intergarion (#184)
    * feat: integration to Zalo and send notification
    
    * update: otp and feedback sender
    
    * update: Zalo webhook receive feedback
    
    * feat: receive feedback and saved

--- src/main/java/com/g42/platform/gms/notification/api/controller/ZaloWebhookController.java
+++ src/main/java/com/g42/platform/gms/notification/api/controller/ZaloWebhookController.java
@@ -1,8 +1,9 @@
 package com.g42.platform.gms.notification.api.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.g42.platform.gms.feedback.api.internal.FeedbackInternalApi;
 import com.g42.platform.gms.notification.api.dto.ZaloWebhookPayloadDto;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @RequestMapping("/api/webhook/zalo")
 public class ZaloWebhookController {
+    @Autowired
+    private FeedbackInternalApi feedbackInternalApi;
     @PostMapping("/events")
     public ResponseEntity<String> receiveZaloEvents(
             @RequestBody ZaloWebhookPayloadDto payload,
@@ -19,14 +22,21 @@ public class ZaloWebhookController {
         System.out.println("Sự kiện: " + payload.getEventName());
 
         try {
-            try {
-                // Dùng ObjectMapper để in JSON ra cho đẹp, dễ nhìn dễ đọc
-                ObjectMapper mapper = new ObjectMapper();
-                String prettyJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(payload);
-                System.out.println(prettyJson);
-            } catch (Exception e) {
-                System.out.println(payload);
+            // 1. Kiểm tra xem có đúng là sự kiện đánh giá dịch vụ không
+            if ("user_feedback".equals(payload.getEventName())) {
+
+                String trackingId = payload.getMessage().getTrackingId(); // Mã phiếu
+                Integer rate = payload.getMessage().getRate(); // Số sao
+                String note = payload.getMessage().getNote(); // Khách ghi chú gì
+
+
+                System.out.printf("Khách hàng đánh giá Phiếu %s: %d Sao. Ghi chú: %s%n", trackingId, rate, note);
+
+                // TODO: Gọi Service để lưu vào bảng Feedback trong Database
+                feedbackInternalApi.addCusFeedbackRespond(rate,note,payload.getMessage().getFeedbacks(),payload.getMessage().getTrackingId(),payload.getMessage().getSubmitTime());
             }
+
+            // Nếu có các event khác (như báo gửi tin thành công/thất bại) thì dùng else if ở đây
 
         } catch (Exception e) {
             // Log lỗi ra nhưng VẪN PHẢI TRẢ VỀ 200 OK cho Zalo
