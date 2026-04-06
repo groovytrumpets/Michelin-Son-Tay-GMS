05ee2f7bb3fd43b9497a4bb119a1bf02bffc8e2b
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-04-06 22:34:18 +0700

    update: googleAuth encode staff role and info to Base64

--- src/main/java/com/g42/platform/gms/security/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/g42/platform/gms/security/OAuth2LoginSuccessHandler.java
@@ -23,6 +23,8 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.List;
 
 @Component
@@ -33,75 +35,89 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
     private final StaffAuthRepo staffAuthRepo;
     private final StaffRoleRepository staffRoleRepository;
 
-    //    @Override
-//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-//        String email = (String) oAuth2User.getAttribute("email");
-//        String googleId = (String) oAuth2User.getAttribute("sub");
-//
-//        String frontendLoginUrl = "http://localhost:5173/login";
-//        String frontendHomeUrl = "http://localhost:5173/dashboard";
-//
-//        StaffAuth staffAuth = staffAuthRepo.searchByEmail(email);
-//        if (staffAuth == null) {
-//            System.err.println("ERROR: Unauthorized Google Login attempt - " + email);
-//            getRedirectStrategy().sendRedirect(request, response, frontendLoginUrl + "?error=USER_NOT_FOUND");
-//            return;
-//        }
-//        if (staffAuth.getStatus().equals("LOCKED")){
-//            getRedirectStrategy().sendRedirect(request, response, frontendLoginUrl + "?error=ACCOUNT_LOCKED");
-//            return;
-//        }
-//
-//        staffAuth.setGoogle_id(googleId);
-//        System.err.println("GOOGLE ID: "+googleId);
-//        staffAuth.setAuthProvider("GOOGLE");
-//        staffAuthRepo.save(staffAuth);
-//        String jwt = jwtService.generateStaffJWToken(staffAuth.getStaffAuthId());
-//
-//        response.sendRedirect(frontendHomeUrl + "?token=" + jwt);
-//    }
-    @Override
+        @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = (String) oAuth2User.getAttribute("email");
         String googleId = (String) oAuth2User.getAttribute("sub");
 
+        String frontendLoginUrl = "http://localhost:5173/login";
+        String frontendHomeUrl = "http://localhost:5173/dashboard";
+
         StaffAuth staffAuth = staffAuthRepo.searchByEmail(email);
         if (staffAuth == null) {
-            System.out.println("ERROR: Staff not found!");
-            //todo: notify that staff 404
-            ResponseEntity<ApiResponse<AuthResponse>> responseResponseEntity = ResponseEntity.ok(ApiResponses.error(AuthErrorCode.USER_NOT_FOUND.name(), "Tài khoản chưa có trong hệ thống"));
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(
-                    new ObjectMapper().writeValueAsString(responseResponseEntity)
-            );
+            System.err.println("ERROR: Unauthorized Google Login attempt - " + email);
+            getRedirectStrategy().sendRedirect(request, response, frontendLoginUrl + "?error=USER_NOT_FOUND");
             return;
         }
-        if (staffAuth.getStatus().equals("LOCKED")) {
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Account locked");
+        if (staffAuth.getStatus().equals("LOCKED")){
+            getRedirectStrategy().sendRedirect(request, response, frontendLoginUrl + "?error=ACCOUNT_LOCKED");
             return;
         }
 
         staffAuth.setGoogle_id(googleId);
-        System.err.println("GOOGLE ID: " + googleId);
+        System.err.println("GOOGLE ID: "+googleId);
         staffAuth.setAuthProvider("GOOGLE");
         staffAuthRepo.save(staffAuth);
-        StaffProfile staffProfile = staffAuth.getStaffProfile();
         String jwt = jwtService.generateStaffJWToken(staffAuth.getStaffAuthId());
-//        response.sendRedirect("http://localhost:3000/oauth2/success?token=" + jwt);
-        AuthResponse authResponse = new AuthResponse("GOOGLE OAUTH2 SUCCESS", "STAFF", jwt);
-        List<String> roles = staffRoleRepository
-                .getStaffRoleByStaff_StaffId(staffProfile.getStaffId())
-                .stream()
-                .map(staffRole -> staffRole.getRole().getRoleCode())
-                .toList();
-        StaffAuthResponse authResponse1 = new StaffAuthResponse(staffProfile.getStaffId(),staffProfile.getFullName(),staffProfile.getAvatar(),"",roles,jwt);
 
-        ResponseEntity<ApiResponse<StaffAuthResponse>> responseResponseEntity = ResponseEntity.ok(ApiResponses.success(authResponse1));
-        response.setContentType("application/json");
-        response.getWriter().write(
-                new ObjectMapper().writeValueAsString(responseResponseEntity)
-        );
+        StaffProfile staffProfile = staffAuth.getStaffProfile();
+            List<String> roles = staffRoleRepository
+                    .getStaffRoleByStaff_StaffId(staffProfile.getStaffId())
+                    .stream()
+                    .map(staffRole -> staffRole.getRole().getRoleCode())
+                    .toList();
+            StaffAuthResponse authResponse = new StaffAuthResponse(staffProfile.getStaffId(),staffProfile.getFullName(),staffProfile.getAvatar(),"",roles,jwt);
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonInfo = mapper.writeValueAsString(authResponse);
+
+            // 2. Mã hóa chuỗi JSON thành Base64 để nó thành một dải chữ và số an toàn
+            String base64Info = Base64.getUrlEncoder().encodeToString(jsonInfo.getBytes(StandardCharsets.UTF_8));
+
+            // 3. Đá về Frontend với chuẩn URL: ?token=...&info=...
+            response.sendRedirect(frontendHomeUrl + "?token=" + jwt + "&info=" + base64Info);
     }
+//    @Override
+//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+//        String email = (String) oAuth2User.getAttribute("email");
+//        String googleId = (String) oAuth2User.getAttribute("sub");
+//
+//        StaffAuth staffAuth = staffAuthRepo.searchByEmail(email);
+//        if (staffAuth == null) {
+//            System.out.println("ERROR: Staff not found!");
+//            //todo: notify that staff 404
+//            ResponseEntity<ApiResponse<AuthResponse>> responseResponseEntity = ResponseEntity.ok(ApiResponses.error(AuthErrorCode.USER_NOT_FOUND.name(), "Tài khoản chưa có trong hệ thống"));
+//            response.setContentType("application/json;charset=UTF-8");
+//            response.getWriter().write(
+//                    new ObjectMapper().writeValueAsString(responseResponseEntity)
+//            );
+//            return;
+//        }
+//        if (staffAuth.getStatus().equals("LOCKED")) {
+//            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Account locked");
+//            return;
+//        }
+//
+//        staffAuth.setGoogle_id(googleId);
+//        System.err.println("GOOGLE ID: " + googleId);
+//        staffAuth.setAuthProvider("GOOGLE");
+//        staffAuthRepo.save(staffAuth);
+//        StaffProfile staffProfile = staffAuth.getStaffProfile();
+//        String jwt = jwtService.generateStaffJWToken(staffAuth.getStaffAuthId());
+////        response.sendRedirect("http://localhost:3000/oauth2/success?token=" + jwt);
+//        AuthResponse authResponse = new AuthResponse("GOOGLE OAUTH2 SUCCESS", "STAFF", jwt);
+//        List<String> roles = staffRoleRepository
+//                .getStaffRoleByStaff_StaffId(staffProfile.getStaffId())
+//                .stream()
+//                .map(staffRole -> staffRole.getRole().getRoleCode())
+//                .toList();
+//        StaffAuthResponse authResponse1 = new StaffAuthResponse(staffProfile.getStaffId(),staffProfile.getFullName(),staffProfile.getAvatar(),"",roles,jwt);
+//
+//        ResponseEntity<ApiResponse<StaffAuthResponse>> responseResponseEntity = ResponseEntity.ok(ApiResponses.success(authResponse1));
+//        response.setContentType("application/json");
+//        response.getWriter().write(
+//                new ObjectMapper().writeValueAsString(responseResponseEntity)
+//        );
+//    }
 }
