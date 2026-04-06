fe6b70f1c91386741502a51b97bc24578483fee3
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-04-06 15:12:41 +0700

    update: saved blog for catalog

--- src/main/java/com/g42/platform/gms/warehouse/infrastructure/specification/WarehouseInternalApiImpl.java
+++ src/main/java/com/g42/platform/gms/warehouse/infrastructure/specification/WarehouseInternalApiImpl.java
@@ -3,6 +3,8 @@ package com.g42.platform.gms.warehouse.infrastructure.specification;
 import com.g42.platform.gms.warehouse.api.dto.CatalogItemDto;
 import com.g42.platform.gms.warehouse.api.internal.WarehouseInternalApi;
 import com.g42.platform.gms.warehouse.domain.entity.CatalogItem;
+import com.g42.platform.gms.warehouse.domain.exception.WarehouseErrorCode;
+import com.g42.platform.gms.warehouse.domain.exception.WarehouseException;
 import com.g42.platform.gms.warehouse.infrastructure.entity.CatalogItemJpa;
 import com.g42.platform.gms.warehouse.infrastructure.mapper.CatalogItemJpaMapper;
 import com.g42.platform.gms.warehouse.infrastructure.repository.CatalogItemJpaRepo;
@@ -21,5 +23,15 @@ public class WarehouseInternalApiImpl implements WarehouseInternalApi {
     public CatalogItemDto getItemInfo(Integer itemId) {
         CatalogItemJpa catalogItemJpa = catalogItemRepo.findById(itemId).orElse(null);
         return catalogItemJpaMapper.toDto(catalogItemJpa);
+    }
+
+    @Override
+    public void updateCatalogBlogService(com.g42.platform.gms.marketing.service_catalog.domain.entity.Service serviceSaved, Integer catalogId) {
+        CatalogItemJpa catalogItemJpa = catalogItemRepo.findById(catalogId).orElse(null);
+        if (catalogItemJpa == null) {
+            throw new WarehouseException("Catalog 404", WarehouseErrorCode.CATALOG_404);
+        }
+        catalogItemJpa.setServiceId(serviceSaved.getServiceId());
+        System.out.println("DEBUG: catalogItem ID: " + catalogItemJpa.getServiceId()+" Saved wth serviceId: " + serviceSaved.getServiceId());
     }
 }
