4149ee735b7cac570a89e9139e49beafca1da763
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-04-08 15:43:31 +0700

    feat: api create stock allocations by estimateId

---
+++ src/main/java/com/g42/platform/gms/estimation/domain/entity/StockAllocation.java
@@ -0,0 +1,28 @@
+package com.g42.platform.gms.estimation.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockAllocation {
+    private Integer allocationId;
+    private Integer serviceTicketId;
+    private Integer estimateItemId;
+    private Integer warehouseId;
+    private Integer itemId;
+    private Integer quantity;
+    private String status;
+    private Integer createdBy;
+    private Instant createdAt;
+    private Instant updatedAt;
+}
---
+++ src/main/java/com/g42/platform/gms/estimation/domain/repository/StockAllocationRepository.java
@@ -0,0 +1,9 @@
+package com.g42.platform.gms.estimation.domain.repository;
+
+import com.g42.platform.gms.estimation.domain.entity.StockAllocation;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockAllocationRepository {
+    StockAllocation createNewAllocation(StockAllocation stockAllocation);
+}
