22f844b6fba2d370341bd4d0dba6388b6892ec4c
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-04-10 17:12:38 +0700

    fix: wrong package and import

--- src/main/java/com/g42/platform/gms/warehouse/app/service/pricing/PricingService.java
+++ src/main/java/com/g42/platform/gms/warehouse/app/service/pricing/PricingService.java
@@ -6,26 +6,23 @@ import com.g42.platform.gms.warehouse.app.service.entry.StockEntryService;
 import com.g42.platform.gms.warehouse.domain.repository.CatalogItemRepo;
 import com.g42.platform.gms.warehouse.infrastructure.entity.WarehousePricingJpa;
 import com.g42.platform.gms.warehouse.infrastructure.repository.WarehousePricingJpaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
 public class PricingService {
+    @Autowired
+    private WarehousePricingJpaRepo warehousePricingJpaRepo;
+    private com.g42.platform.gms.catalog.service.CatalogItemService catalogItemService;
+    @Autowired
+    private CatalogItemRepo catalogItemRepo;
+    @Autowired
+    private CatalogDtoMapper catalogDtoMapper;
+    @Autowired
+    private StockEntryService stockEntryService;
 
-    private final WarehousePricingJpaRepo warehousePricingJpaRepo;
-    private final com.g42.platform.gms.catalog.application.service.CatalogItemService catalogItemService;
-    private final CatalogItemRepo catalogItemRepo;
-    private final CatalogDtoMapper catalogDtoMapper;
-    private final StockEntryService stockEntryService;
-
-    public PricingService(WarehousePricingJpaRepo warehousePricingJpaRepo, com.g42.platform.gms.catalog.application.service.CatalogItemService catalogItemService, CatalogItemRepo catalogItemRepo, CatalogDtoMapper catalogDtoMapper, StockEntryService stockEntryService) {
-        this.warehousePricingJpaRepo = warehousePricingJpaRepo;
-        this.catalogItemService = catalogItemService;
-        this.catalogItemRepo = catalogItemRepo;
-        this.catalogDtoMapper = catalogDtoMapper;
-        this.stockEntryService = stockEntryService;
-    }
 
     public BigDecimal getEffectivePrice(Integer itemId, Integer warehouseId, BigDecimal price) {
         //warehousePricing
