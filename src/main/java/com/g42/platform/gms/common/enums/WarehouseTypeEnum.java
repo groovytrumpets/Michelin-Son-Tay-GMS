0fc671eef53ec5921084f96a4d150ce8882cd0ac
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-04-13 10:46:19 +0700

    Update/service ticket (#234)
    * feat: getTicketHistoryBy Customer and Vehicle Id
    
    * update: estimate item have unit
    
    * update: bookingRequestManage
    
    * update: bookingRequestManage
    
    * update: bookingRequestManage
    
    * update: bookingRequestManage
    
    * fix: history
    
    * fix: history
    
    * fix: history
    
    * fix: stock quantity
    
    * feat: warehouse get All
    
    * feat: warehouse get All
    
    * fix: warehouse enum

--- src/main/java/com/g42/platform/gms/common/enums/WarehouseTypeEnum.java
+++ src/main/java/com/g42/platform/gms/common/enums/WarehouseTypeEnum.java
@@ -2,10 +2,6 @@ package com.g42.platform.gms.common.enums;
 
 public enum WarehouseTypeEnum {
     //enum('MASTER', 'BRANCH')
-    DRAFT,
-    SENT,
-    APPROVED,
-    REJECTED,
-    ARCHIVED,
-    CANCELLED
+    MASTER,
+    BRANCH,
 }
