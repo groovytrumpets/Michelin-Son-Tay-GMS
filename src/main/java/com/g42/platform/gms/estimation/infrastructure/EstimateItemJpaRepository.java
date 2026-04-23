85c6f19e1edaa4bc571fd8dccbf2b505353b4b4c
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-04-23 23:24:33 +0700

    fix: allocation estimate version update fix linking

--- src/main/java/com/g42/platform/gms/estimation/infrastructure/EstimateItemJpaRepository.java
+++ src/main/java/com/g42/platform/gms/estimation/infrastructure/EstimateItemJpaRepository.java
@@ -3,6 +3,7 @@ package com.g42.platform.gms.estimation.infrastructure;
 import com.g42.platform.gms.estimation.domain.entity.EstimateItem;
 import com.g42.platform.gms.estimation.infrastructure.entity.EstimateItemJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +11,9 @@ interface EstimateItemJpaRepository extends JpaRepository<EstimateItemJpa, Integ
     List<EstimateItemJpa> findByEstimateId(Integer estimateId);
 
     List<EstimateItemJpa> findByEstimateIdAndIsRemoved(Integer estimateId, Boolean isRemoved);
+    @Query("""
+        select ei from EstimateItemJpa ei,EstimateJpa e where ei.estimateId=e.id 
+                and e.serviceTicketId=:serviceTicketId
+        """)
+    List<EstimateItemJpa> findByServiceTicketId(Integer serviceTicketId);
 }
