7f1f04c125df1d5ccd2e7de97c7cc5e153abe353
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-04-24 11:58:14 +0700

    update: get warehouse by serviceticket id (#276)

---
+++ src/main/java/com/g42/platform/gms/estimation/api/dto/WarehouseDto.java
@@ -0,0 +1,25 @@
+package com.g42.platform.gms.estimation.api.dto;
+
+import com.g42.platform.gms.common.enums.WarehouseTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WarehouseDto {
+    private Integer warehouseId;
+    private String warehouseCode;
+    private String warehouseName;
+    private WarehouseTypeEnum warehouseType;
+    private Integer parentWarehouseId;
+    private String address;
+    private Integer managerStaffId;
+    private Boolean isActive;
+    private Instant createdAt;
+}
