59ba7b8732812d550c20c853315a5bab1c62b811
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-04-25 11:18:55 +0700

    fix: create allocation for removed item

--- src/main/java/com/g42/platform/gms/estimation/app/service/StockAllocationService.java
+++ src/main/java/com/g42/platform/gms/estimation/app/service/StockAllocationService.java
@@ -110,7 +110,7 @@ public class StockAllocationService {
         else {
             List<EstimateItem> estimateItems = estimateItemRepository.findByEstimateId(estimateId);
             for (EstimateItem estimateItem : estimateItems) {
-                if (estimateItem.getItemId() != null && estimateItem.getWarehouseId() != null) {
+                if (estimateItem.getItemId() != null && estimateItem.getWarehouseId() != null && estimateItem.getIsRemoved()==false) {
                     StockAllocation stockAllocation = new StockAllocation();
                     stockAllocation.setServiceTicketId(newEstimate.getServiceTicketId());
                     stockAllocation.setEstimateItemId(estimateItem.getId());
