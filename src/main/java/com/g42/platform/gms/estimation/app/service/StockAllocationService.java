f6503f2d53916c9fd765b89718e063ad091e6185
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2026-04-25 10:58:26 +0700

    fix: create allocation for removed item

--- src/main/java/com/g42/platform/gms/estimation/app/service/StockAllocationService.java
+++ src/main/java/com/g42/platform/gms/estimation/app/service/StockAllocationService.java
@@ -75,7 +75,8 @@ public class StockAllocationService {
                     .map(StockAllocation::getEstimateItemId)
                     .collect(Collectors.toSet());
             List<EstimateItem> brandNewItems = newEstimateItems.stream()
-                    .filter(newItem -> newItem.getRevisedFromItemId() == null)
+                    .filter(newItem -> newItem.getRevisedFromItemId() == null
+                            && newItem.getIsRemoved()==false)
                     .toList();
             for (EstimateItem createItem : brandNewItems) {
                 System.out.println("estimate cr: "+createItem.getEstimateId());
