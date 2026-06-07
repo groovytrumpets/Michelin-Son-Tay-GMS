ea2106a677c69497e76afdf4ae1d43f1494f3f65
Nguyen Nam Khanh <166611562+groovytrumpets@users.noreply.github.com>
2026-06-07 18:50:55 +0700

    Update/warehouse rework (#365)
    * fix: catalog create product line null bug
    
    * fix: service update and editing
    
    * fix: service update and editing

--- src/main/java/com/g42/platform/gms/warehouse/infrastructure/mapper/CatalogItemJpaMapper.java
+++ src/main/java/com/g42/platform/gms/warehouse/infrastructure/mapper/CatalogItemJpaMapper.java
@@ -4,11 +4,16 @@ import com.g42.platform.gms.warehouse.api.dto.CatalogItemDto;
 import com.g42.platform.gms.warehouse.domain.entity.CatalogItem;
 import com.g42.platform.gms.warehouse.infrastructure.entity.CatalogItemJpa;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CatalogItemJpaMapper {
+    @Mapping(source = "serviceId", target = "serviceServiceId")
     CatalogItem toDomain(CatalogItemJpa catalogItemJpa );
+
+    @Mapping(source = "serviceServiceId", target = "serviceId")
     CatalogItemJpa toJpa(CatalogItem catalogItem);
 
+    @Mapping(source = "serviceId", target = "serviceServiceId")
     CatalogItemDto toDto(CatalogItemJpa catalogItemJpa);
 }
